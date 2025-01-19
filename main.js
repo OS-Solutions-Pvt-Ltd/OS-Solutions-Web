a10c251e6d91277fc0923f833b6a503f5b1d7cd3
Sameera Madushan Silva <madushansameera499@gmail.com>
2025-01-20 01:04:01 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Responsive Navbar Functionality
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navLinks.classList.toggle("active");
+    });
+});
