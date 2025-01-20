e1d44c79efdfa80c409bb39952f83b3885bf6709
Sameera Madushan Silva <madushansameera499@gmail.com>
2025-01-20 17:10:47 +0530

    send mail add

--- main.js
+++ main.js
@@ -10,3 +10,18 @@ window.onscroll = () => {
     menu.classList.remove("bx-x");
     navbar.classList.remove("active");
 };
+
+
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const telephone = document.getElementById("telephone").value;
+    const message = document.getElementById("message").value;
+
+    // Create a mailto link
+    const mailtoLink = `mailto:madushansameera499@gmail.com?subject=New Project Inquiry&body=Name: ${name}%0D%0AEmail: ${email}%0D%0AContact Number: ${telephone}%0D%0AMessage: ${message}`;
+    window.location.href = mailtoLink;
+});
+
